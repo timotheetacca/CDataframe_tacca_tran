e2ec95ba6a273087d81be5ac7f08a6c2236e05d2
timotheetacca <66522009+timotheetacca@users.noreply.github.com>
2024-04-07 17:35:11 +0200

    Fixed memory problem with fill_cdataframe()

--- main.c
+++ main.c
@@ -8,5 +8,11 @@ int main() {
     insert_value(mycol, 44);
     insert_value(mycol, 15);
 
+    print_col(mycol);
+
+    CDATAFRAME* cdataframe = create_cdataframe();
+    fill_cdataframe(&cdataframe);
+    display_cdataframe(&cdataframe);
+
     return 0;
 }
