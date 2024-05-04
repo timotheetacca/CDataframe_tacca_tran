8999c52ced156be997cdc2d8a0dfb718f9a9736a
timotheetacca <66522009+timotheetacca@users.noreply.github.com>
2024-05-04 17:35:25 +0200

    Fixed quicksort()

--- main.c
+++ main.c
@@ -13,30 +13,6 @@ int main() {
     sort(cdataframe->columns[0]);
     print_col_by_index(cdataframe->columns[0]);
 
-    add_row_to_cdataframe(cdataframe);
-    sort(cdataframe->columns[0]);
-    print_col_by_index(cdataframe->columns[0]);
-
-    // Add column
-    add_column_to_cdataframe(cdataframe);
-
-    // Delete column
-    delete_column_from_cdataframe(cdataframe, 0);
-
-    // Check existence of value
-    ENUM_TYPE value_type = INT;
-    int value = 0;
-    printf("%d", check_if_value_exists(cdataframe, value_type, &value));
-
-    // Access value
-    void* accessed_value = access_value(cdataframe, 0, 0);
-    int* int_ptr = (int*)accessed_value;
-    printf("%d\n", *int_ptr);
-
-    // Replace value
-    ENUM_TYPE new_value_type = INT;
-    int new_value = 0;
-    replace_value(cdataframe, 0, 0, new_value_type, &new_value);
 
 
     return 0;
