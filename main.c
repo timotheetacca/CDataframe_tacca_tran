92286b322c94763f8d847218062a66f19a5c801d
Kim Lan TRAN <150260333+Kimi215@users.noreply.github.com>
2024-05-05 13:28:44 +0200

    Reloaded part 1 files

--- main.c
+++ main.c
@@ -1,54 +1,52 @@
 #include "column.h"
 #include "CDataframe.h"
-#include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
+    COLUMN *mycol = create_column("My column");
+
+    insert_value(mycol, 52);
+    insert_value(mycol, 44);
+    insert_value(mycol, 15);
+
+    print_col(mycol);
+
     CDATAFRAME* cdataframe = create_cdataframe();
     fill_cdataframe(cdataframe);
     display_cdataframe(cdataframe);
 
-    // Print rows 0-1
-    display_limited_rows_cdataframe(cdataframe, 0, 1);
-    printf("\n");
+    display_limited_rows_cdataframe(cdataframe, 2, 3);
+    display_limited_columns_cdataframe(cdataframe, 2, 3);
 
-    // Print columns 0-1
-    display_limited_columns_cdataframe(cdataframe, 0,1);
+    display_number_of_rows(cdataframe);
+    display_number_of_columns(cdataframe);
+    display_name_of_columns(cdataframe);
 
-    // Add row
+    printf("%d\n", count_cells_condition(cdataframe, 1, '='));
+    printf("%d\n", count_cells_condition(cdataframe, 4, '>'));
+    printf("%d\n", count_cells_condition(cdataframe, 4, '<'));
+
+    add_column_to_cdataframe(cdataframe);
+    rename_column(cdataframe, 0, "Col1");
+    delete_column_to_cdataframe(cdataframe, 1);
+    display_cdataframe(cdataframe);
+
+    delete_row_from_cdataframe(cdataframe, 0);
+    display_cdataframe(cdataframe);
+
     add_row_to_cdataframe(cdataframe);
     display_cdataframe(cdataframe);
 
-    // Delete row
-    delete_row_from_cdataframe(cdataframe, 1);
+    printf("%d\n", check_if_value_exists(cdataframe, 1));
+    printf("%d\n", check_if_value_exists(cdataframe, 1234));
+
+    printf("%d\n", access_value(cdataframe, 0, 0));
+    replace_value(cdataframe, 0, 0, 4);
     display_cdataframe(cdataframe);
 
-    // Add column
-    add_column_to_cdataframe(cdataframe);
-
-    // Delete column
-    delete_column_from_cdataframe(cdataframe, 0);
-
-    // Rename column
-    rename_column(cdataframe,0, "Column 0");
-
-    // Check existence of value
-    ENUM_TYPE value_type = INT;
-    int value = 0;
-    printf("%d", check_if_value_exists(cdataframe, value_type, &value));
-
-    // Replace value
-    ENUM_TYPE new_value_type = INT;
-    int new_value = 0;
-    replace_value(cdataframe, 0, 0, new_value_type, &new_value);
-
-    // Comparison with x
-    ENUM_TYPE x_type1 = INT;
-    int x1=1,x2=4,x3=50;
-    printf("%d\n", count_cells_condition(cdataframe, x_type1, &x1, '='));
-    printf("%d\n", count_cells_condition(cdataframe, x_type1, &x2, '>'));
-    printf("%d\n", count_cells_condition(cdataframe, x_type1, &x3, '<'));
+    hard_fill_cdataframe(cdataframe);
+    display_cdataframe(cdataframe);
 
     return 0;
 }
+
